06ecfe16ca750aef7bb9c4c26225bd75631f2efa
Pelin Yiğit <pelinyigit@yologamestudio.com>
2022-04-09 20:33:48 +0300

    Rank Player and Opponents

--- Assets/Scripts/RankController.cs
+++ Assets/Scripts/RankController.cs
@@ -7,32 +7,60 @@ using UnityEngine.AI;
 public class RankController : MonoBehaviour
 {
     private GameObject player;
+    private GameObject temp;
+    private float playerPos;
+    private int playerRank = 7;
 
     public TextMeshProUGUI tmp;
     public GameObject[] opponents;
 
     void Start()
     {
-        player = FindObjectOfType<CharacterController>().gameObject;    
+        tmp.text = playerRank.ToString();
+        player = FindObjectOfType<CharacterController>().gameObject;
     }
 
     void Update()
     {
         if (player.GetComponent<CharacterController>().isGameStarted)
         {
-            RankCharacters();
+            playerPos = player.transform.position.z;
+            SortOpponents();
+        }
+    }
+
+    //Bubble Sort
+    private void SortOpponents()
+    {
+        for (int j = 0; j < opponents.Length; j++)
+        {
+            for (int i = 0; i < opponents.Length - 1; i++)
+            {
+                if (opponents[i].transform.localPosition.z > opponents[i + 1].transform.localPosition.z)
+                {
+                    temp = opponents[i + 1];
+                    opponents[i + 1] = opponents[i];
+                    opponents[i] = temp;
+
+                    RankCharacters();
+                }
+            }
         }
     }
 
     private void RankCharacters()
     {
-        foreach (GameObject opponentRank in opponents)
-        {
-            for (int i = 0; i < opponents.Length; i++)
-            {
+        playerRank = Mathf.Clamp(playerRank, 2, 10);
 
-            }
+        if (playerPos > temp.transform.localPosition.z)
+        {
+            playerRank -= 1;
+            tmp.text = playerRank.ToString();
         }
-        //tmp.text = rank
+        else
+        {
+            playerRank += 1;
+            tmp.text = playerRank.ToString();
+        }
     }
 }
